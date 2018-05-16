dfda5ee532faa48af9d5c8f2e0def7fe83297e69
PoteXB <baishuangchun@sina.com>
2018-05-16 22:23:18 +0800

    抖音

--- chajian/js/main.js
+++ chajian/js/main.js
@@ -1,4 +1,4 @@
-﻿$("<link>").attr({rel:"stylesheet",type:"text/css",href:chrome.runtime.getURL("css/main.css")}).appendTo("head");
+﻿// $("<link>").attr({rel:"stylesheet",type:"text/css",href:chrome.runtime.getURL("css/main.css")}).appendTo("head");
 // $("<link>").attr({rel:"stylesheet",type:"text/css",href:chrome.runtime.getURL("css/main1212.css")}).appendTo("head");
 // $("<link>").attr({rel:"stylesheet",type:"text/css",href:chrome.runtime.getURL("css/mainList.css")}).appendTo("head");
 chrome.storage.local.get("zqwmainData",function (data) {
